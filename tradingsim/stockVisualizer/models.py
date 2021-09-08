4bc3a2dac33d89b84796eb3aab115b71324ead4b
rrondir <roccorondinella@gmail.com>
2021-09-08 22:59:51 +0100

    Initial commit

---
+++ tradingsim/stockVisualizer/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+class StockData(models.Model):
+    symbol = models.TextField(null=True)
+    data = models.TextField(null=True)
